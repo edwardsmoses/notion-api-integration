1a0c8cb5c2897257e599df2e9da5d833d3afbc5e
Edwards Moses <edwardsmoses3@gmail.com>
2021-06-13 17:35:22 +0100

    Fixed error: 'Uncaught TypeError: Cannot read property 'process' of undefined"
    https://github.com/vitejs/vite/issues/1973#issuecomment-787571499

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import './style.css';
 
-const api = import.meta.env.VITE_API_KEY;
+import { Client } from "@notionhq/client"
+const apiKey = import.meta.env.VITE_API_KEY;
 const databaseId = import.meta.env.VITE_DATABASE_ID;
 
+
+const getResources = () => {
+  const notion = new Client({auth: apiKey});
+  return [{
+    title: "Sample Resource added with Notion API",
+    url: "https://google.com",
+  }]
+}
+
 const app = document.querySelector<HTMLDivElement>('#app')!
-console.log(api);
+console.log(getResources());
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>

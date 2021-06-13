384425af379d583699ded1972d97eaa44efb579f
Edwards Moses <edwardsmoses3@gmail.com>
2021-06-13 18:09:20 +0100

    Wow. there's a problem with notionapi module

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,16 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import { Client } from "@notionhq/client";
+import { useEffect } from 'react';
+
 export default function Home() {
+
+  useEffect(() => {
+    const notion = new Client({auth: process.env.NEXT_API_KEY});
+    console.log(notion.pages);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
